edad34096844aa65eb66345a9249a66f9b60d15e
Trent Nicholson <tnicholson@abacusbio.co.nz>
2020-01-16 13:39:20 +1300

    Add powershell profile

--- updateDotfiles.cmd
+++ updateDotfiles.cmd
@@ -3,6 +3,8 @@ set EDITOR=nvim
 rem Copy files
 robocopy *.vim C:\Users\TrentNicholson\AppData\Local\nvim .
 
+robocopy *.ps1 C:\Users\TrentNicholson\Documents\WindowsPowerShell .
+
 rem Add and commit
 git add .
 git commit -v
